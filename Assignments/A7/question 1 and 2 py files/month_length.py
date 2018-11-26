ae593684fa5bc236c685353c98e919596dd5b520
Tianxin Zheng <txzheng@uchicago.edu>
2018-11-26 11:38:23 -0600

    coverage reports

--- Assignments/A7/question 1 and 2 py files/month_length.py
+++ Assignments/A7/question 1 and 2 py files/month_length.py
@@ -2,7 +2,7 @@ def month_length(month, leap_year=False):
 	if month in {"September", "April", "June", "November"}:
 		return 30
 	elif month in {"January", "March", "May", "July", \
-	               "August"， "October", "December"}:
+	               "August", "October", "December"}:
 	    return 31
 	if month == "February":
 		if not leap_year:
